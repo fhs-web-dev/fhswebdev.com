f6b7bbfcb182a66fabfc163d6eee5c41ed339efd
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-23 22:40:38 -0600

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Global CSS
+// normalize.css fixes css issues brought about by browsers; it makes styling easier
+import 'normalize.css/normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
