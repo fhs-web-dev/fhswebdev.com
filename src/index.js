98924726d71112f77e349a1bb1ec66ee6321402f
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-24 13:57:11 -0600

    Add custom reset css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 // Global CSS
 // normalize.css fixes css issues brought about by browsers; it makes styling easier
 import 'normalize.css/normalize.css';
+import './reset.css';
 import './index.css';
+
 import App from './App';
+// Service Worker is for offline browser use
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
