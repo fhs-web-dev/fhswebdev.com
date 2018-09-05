2aeb7dc323cf331533bd71a76d415abed1c9353b
Kyle Pfromer <kpfromer2@gmail.com>
2018-09-04 19:58:46 -0600

    Cleanup App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-// Components
-// import Header from "./components/Header/Header";
-// import Footer from "./components/Footer/Footer";
-
 import styles from './App.module.css';
-import MediaQuery from "react-responsive";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LockOnToMenu from "./components/LockOnToMenu/LockOnToMenu";
 import Home from "./components/Pages/Home/Home";
-// import RegularPages from './components/Pages/RegularPages/RegularPages';
 
 class App extends Component {
 
@@ -22,7 +15,6 @@ class App extends Component {
 
   render() {
     const { people } = this.props;
-    const { displayMenu } = this.state;
     return (
       <BrowserRouter>
         <div className="App">
@@ -31,8 +23,6 @@ class App extends Component {
               <Route render={() => (
                 <Home people={people} />
               )} />
-              {/* <Route exact path="/" component={Home} /> */}
-              {/* <RegularPages displayMenu={displayMenu} toggleMenu={this.handleMenu} /> */}
             </Switch>
           </div>
         </div>
